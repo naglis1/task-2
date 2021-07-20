b64d7cc51bdafc823d1b548f910697bd97a9c54f
Naglis Šuliokas <naglis1134@gmail.com>
2021-07-20 11:28:40 +0300

    Adding files

---
+++ InOut.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class InOut 
+    {
+        public void Output(int depth)
+        {
+            Console.WriteLine(depth);
+        }
+
+        public void Input(Saka root, int[] data)
+        {
+            int k = 0;
+            CreateTree(root, data, ref k);
+        }
+        
+        private void CreateTree(Saka saka, int[] data, ref int k)
+        {
+            //Adding nodes recursively in DepthFirstSearch-like order 
+            int num = data[k];
+            saka.sakos = new List<Saka>();
+            for (int i = 0; i < num; i++)
+            {
+                saka.sakos.Add(new Saka());
+                k++;
+                CreateTree(saka.sakos[i], data, ref k);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Saka
+    {
+        public List<Saka> sakos;
+    }
+
+    class Program
+    {
+        Program()
+        {
+            InOut io = new InOut();
+            Algorithm algo = new Algorithm();
+
+            root = new Saka();
+            io.Input(root, data);
+            
+            depth = algo.FindDepth(root);
+            io.Output(depth);
+        }
+
+        Saka root;
+        int depth;
+        //Data of tree example given in the task. Shows node's number of children in DepthFirstSearch-like order
+        readonly int[] data = { 2, 1, 0, 3, 1, 0, 2, 1, 0, 0, 0 };
+        
+        static void Main(string[] args)
+        {
+            new Program();  
+        }
+    }
+}
