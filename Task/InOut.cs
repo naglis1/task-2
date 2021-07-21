b94ea1d6fc5278145833b152c6755c9a4582199e
Naglis Šuliokas <naglis1134@gmail.com>
2021-07-21 23:50:45 +0300

    Added unit testing

---
+++ Task/InOut.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class InOut
+    {
+        public void OutputTreeDepth(int depth)
+        {
+            Console.WriteLine(depth);
+        }
+
+        public void CreateTree(Saka root, int[] data)
+        {
+            int k = 0;
+            CreateSubtree(root, data, ref k);
+        }
+
+        private void CreateSubtree(Saka saka, int[] data, ref int k)
+        {
+            //Adding nodes recursively in DepthFirstSearch-like order 
+            int num = data[k];
+            saka.sakos = new List<Saka>();
+            for (int i = 0; i < num; i++)
+            {
+                saka.sakos.Add(new Saka());
+                k++;
+                CreateSubtree(saka.sakos[i], data, ref k);
+            }
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Saka
+    {
+        public List<Saka> sakos;
+    }
+
+    public class Program
+    {
+        readonly Saka root;
+        public int depth;
+
+        public Program(int[] data)
+        {
+            InOut io = new InOut();
+            Algorithm algo = new Algorithm();
+
+            root = new Saka();
+            io.CreateTree(root, data);
+            
+            depth = algo.FindDepth(root);
+            io.OutputTreeDepth(depth);
+        }
+
+        static void Main(string[] args)
+        {
+            //Data of tree example given in the task
+            //Shows node's number of children in DepthFirstSearch-like order
+            //Other examples are in test project
+            //Tree cannot be empty
+            int[] data = { 2, 1, 0, 3, 1, 0, 2, 1, 0, 0, 0 };
+            new Program(data);  
+        }
+    }
+}
